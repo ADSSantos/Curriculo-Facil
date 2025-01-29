8e91eb8175a2b296e4972558076c186e6ea08924
ADSSantos <121822984+ADSSantos@users.noreply.github.com>
2025-01-29 11:11:24 +0000

    config inicial

---
+++ curriculo-facil/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Nunito, Nunito_Sans } from "next/font/google";
+import "./globals.css";
+import cn from "classnames";
+
+const fontSans = Nunito_Sans({ subsets: ["latin"], variable: "--font-sans" });
+const fontTitle = Nunito({ subsets: ["latin"], variable: "--font-title" });
+
+export const metadata: Metadata = {
+  title: "Curriculo Facil",
+  description: "#",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={cn(fontTitle.variable, fontSans.variable)}>
+        {children}
+      </body>
+    </html>
+  );
+}
