1e508d54d018636dbf8fad8bb11f602d6b98d06b
ADSSantos <121822984+ADSSantos@users.noreply.github.com>
2025-02-05 07:43:04 +0000

    nav bar

--- curriculo-facil/next.config.mjs
+++ curriculo-facil/next.config.mjs
@@ -1,4 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: false,
+    // Adiciona a configuração para desabilitar os overlays de erro
+    devIndicators: {
+        buildActivity: false,
+    },
+    webpack(config) {
+        const fileLoaderRule = config.module.rules.find(
+            (rule) => rule.test && rule.test.test && rule.test.test(".svg")
+        );
+
+        if (fileLoaderRule) {
+            config.module.rules.push(
+                {
+                    ...fileLoaderRule,
+                    test: /\.svg$/i,
+                    resourceQuery: /url/,
+                },
+                {
+                    test: /\.svg$/i,
+                    issuer: fileLoaderRule.issuer,
+                    resourceQuery: { not: [...(fileLoaderRule.resourceQuery?.not || []), /url/] },
+                    use: ["@svgr/webpack"],
+                }
+            );
+
+            fileLoaderRule.exclude = /\.svg$/i;
+        }
+
+        return config;
+    },
+};
 
 export default nextConfig;
---
+++ curriculo-facil/src/app/dashboard/layout.tsx
@@ -0,0 +1,32 @@
+
+import { ReactNode } from "react";
+import Logo from "../../assets/logo.svg";
+
+
+
+type DashboardLayoutProps = {
+  children: ReactNode;
+};
+
+export default function DashboardLayout({ children }: DashboardLayoutProps) {
+  return (
+    <div className="w-full h-screen overflow-hidden grid grid-cols-[300px,1fr]">
+      <div className="w-full h-full flex flex-col items-center border-r border-muted">
+        <div className="w-full p-6 border-b border-muted">
+          <Logo className="max-w-[100px] mx-auto" />
+        </div>
+      </div>
+
+      <main>{children}</main>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
