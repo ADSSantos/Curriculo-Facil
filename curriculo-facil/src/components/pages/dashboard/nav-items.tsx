2c6cee71ded45a1fb795900b836553cb86346d8f
ADSSantos <121822984+ADSSantos@users.noreply.github.com>
2025-02-06 16:08:15 +0000

    button end icon navitems

--- curriculo-facil/src/components/pages/dashboard/nav-items.tsx
+++ curriculo-facil/src/components/pages/dashboard/nav-items.tsx
@@ -1,10 +1,14 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import { Newspaper, SquareUser } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 
 
 export const NavItems = () => {
+  const pathname = usePathname();
   const navItems = [
     {
       label: "Curriculos",
@@ -20,9 +24,12 @@ export const NavItems = () => {
   return (
     <nav className="w-full flex flex-col gap-2 px-2 py-4">
       {navItems.map((item) => {
+        const isActive = pathname.startsWith(item.path);
         return (
         <Link key={item.path} href={item.path}>
-            <Button variant="ghost" className="w-full gap-2 justify-start">
+            <Button variant="ghost" className={cn("w-full gap-2 justify-start",
+              isActive && "bg-accent"
+            )}>
               <item.icon size={16} />
                 {item.label}
             </Button>
