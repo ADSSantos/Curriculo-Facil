16bef7cf0b2ca40c2d463fc63525de75ec036e91
ADSSantos <121822984+ADSSantos@users.noreply.github.com>
2025-02-06 13:41:06 +0000

    navItems

---
+++ curriculo-facil/src/app/components/pages/dashboard/nav-items.tsx
@@ -0,0 +1,5 @@
+export const NavItems = () => {
+    return (
+    <nav className="w-full flex flex-col gap-2 px-2 py-4"></nav>
+    )
+}
--- curriculo-facil/src/app/dashboard/layout.tsx
+++ curriculo-facil/src/app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 
 import { ReactNode } from "react";
 import Logo from "../../assets/logo.svg";
+import { NavItems } from "../components/pages/dashboard/nav-items";
 
 
 
@@ -11,11 +12,13 @@ type DashboardLayoutProps = {
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
     <div className="w-full h-screen overflow-hidden grid grid-cols-[300px,1fr]">
-      <div className="w-full h-full flex flex-col items-center border-r border-muted">
+      <aside className="w-full h-full flex flex-col items-center border-r border-muted">
         <div className="w-full p-6 border-b border-muted">
           <Logo className="max-w-[100px] mx-auto" />
         </div>
-      </div>
+
+        <NavItems/>
+      </aside>
 
       <main>{children}</main>
     </div>
