395dba45b52c2ddadb5034f23403989b7b0ddae4
ADSSantos <121822984+ADSSantos@users.noreply.github.com>
2025-02-20 15:53:19 +0000

    infos sidebar

--- curriculo-facil/src/app/dashboard/(layout)/layout.tsx
+++ curriculo-facil/src/app/dashboard/(layout)/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import Logo from "../../assets/logo.svg";
+import Logo from "../../../assets/logo.svg";
 import { NavItems } from "@/components/pages/dashboard/nav-items";
 import { UserDropdown } from "@/components/pages/dashboard/user-dropdown";
 
--- curriculo-facil/src/components/pages/dashboard/resume/index.tsx
+++ curriculo-facil/src/components/pages/dashboard/resume/index.tsx
@@ -1,7 +1,34 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { InfosSidebar } from "./infos-sideBar";
+import { ResumeContent } from "./resume-content";
+import { StructureSidebar } from "./structure-sidebar";
 export const ResumePage = () => {
-    return (
-        <div>
-            <h1>Resume okkkkkkkk</h1> 
-        </div>
-    )
-}
+  return (
+    <main className="w-full h-screen overflow-y-hidden">
+      
+      <ResizablePanelGroup direction="horizontal" className=" w-full h-full">
+        <ResizablePanel minSize={20} maxSize={40} defaultSize={30}>One
+            <InfosSidebar/>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+
+
+        <ResizablePanel className="h-full">Two
+            <ResumeContent/>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+
+        <ResizablePanel minSize={20} maxSize={35} defaultSize={25} className="h-full">Tree
+            <StructureSidebar/>
+        </ResizablePanel>
+        <ResizableHandle  />
+
+      </ResizablePanelGroup>
+
+    </main>
+  );
+};
